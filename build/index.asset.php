fb42eacca8d5de85c56bc502df20c95322bd520b
Dominic German <dominicgerman@gmail.com>
2023-06-07 14:51:07 -0500

    Build step 6.7.23

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7779e2d6f478ba0b45dd');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '3532881633847a8874fb');
